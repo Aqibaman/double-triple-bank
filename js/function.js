33f73837695df200fe63e4bdb2b3b20a038a4c29
Aqibaman <aqibaman0173@gmail.com>
2022-08-18 19:50:01 +0600

    first commit

---
+++ js/function.js
@@ -0,0 +1,43 @@
+function getInputFieldValueById(inputFieldId) {
+    const inputField = document.getElementById(inputFieldId);
+    const inputFieldValueString = inputField.value;
+    const inputFieldValue = parseFloat(inputFieldValueString);
+    inputField.value = "";
+    return inputFieldValue;
+}
+
+function getElementValueById(elementFieldId) {
+    const elementTotal = document.getElementById(elementFieldId);
+    const previousElementTotalString = elementTotal.innerText;
+    const previousElementTotal = parseFloat(previousElementTotalString);
+    return previousElementTotal;
+}
+
+function setTextElementValueById(elementFieldId, newValue) {
+    const textEelment = document.getElementById(elementFieldId);
+    textEelment.innerText = newValue;
+}
+
+document.getElementById("btn-both").addEventListener("click", function () {
+    const newAmount = getInputFieldValueById("both-input");
+    const previousDoubleTotal = getElementValueById("double-total");
+    const newDoubleTotal = previousDoubleTotal + (newAmount * 2);
+    setTextElementValueById("double-total", newDoubleTotal);
+    const previousTripleTotal = getElementValueById("triple-total");
+    const newTripleTotal = previousTripleTotal + (newAmount * 3);
+    setTextElementValueById("triple-total", newTripleTotal);
+})
+
+document.getElementById("btn-double").addEventListener("click", function () {
+    const newDoubleAmount = getInputFieldValueById("double-input");
+    const previousDoubleTotal = getElementValueById("double-total");
+    const newDoubleTotal = previousDoubleTotal + (newDoubleAmount * 2);
+    setTextElementValueById("double-total", newDoubleTotal);
+})
+
+document.getElementById("btn-triple").addEventListener("click", function () {
+    const newTripleAmount = getInputFieldValueById("triple-input");
+    const previousTripleTotal = getElementValueById("triple-total");
+    const newTripleTotal = previousTripleTotal + (newTripleAmount * 3);
+    setTextElementValueById("triple-total", newTripleTotal);
+})
